ceb46870d957fa5fb759d696f9d8e6efb3e386ac
Memphis1983 <kiran.iyer83@gmail.com>
2021-08-12 02:25:48 +0530

    update 4

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const passport = require('passport')
-const config = require('../config')
+const config = require('./config/config')
 const router = express.Router()
 
 // hello world
